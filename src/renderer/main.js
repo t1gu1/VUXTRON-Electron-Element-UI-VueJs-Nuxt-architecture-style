a2062d16695217ff2bfbb1de6b63e1096a31febb
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2019-02-22 13:52:14 -0500

    Add plugin folder

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,15 +5,6 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-
-import ElementUI from 'element-ui';
-import locale from 'element-ui/lib/locale/lang/en';
-
-// element-ui style
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(ElementUI, { locale });
-
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
@@ -25,3 +16,7 @@ new Vue({
   store,
   template: '<App/>'
 }).$mount('#app')
+
+
+// Plugins import section
+require('./plugins/element-ui')
---
+++ src/renderer/plugins/element-ui.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
+
+// element-ui style
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI, { locale });
