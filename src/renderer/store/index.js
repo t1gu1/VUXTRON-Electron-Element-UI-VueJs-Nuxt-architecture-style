8dd0cfb2bfa6e570a54a7544a404d5c24622bfe6
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2019-02-26 03:30:46 -0500

    Web build works now!!! :D 🔥🔥🔥

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import { createPersistedState, createSharedMutations } from 'vuex-electron'
-
 import modules from './modules'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules,
-  plugins: [
-    createPersistedState(),
-    //createSharedMutations()
+  plugins: process.env.IS_WEB ? [] : [
+    require('vuex-electron').createPersistedState(),
+    //require('vuex-electron').createSharedMutations()
   ],
   strict: process.env.NODE_ENV !== 'production'
 })
